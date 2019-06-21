62fa840ad1dac1edbd616925d81007e402cc658e
suigang <244153653@qq.com>
2019-06-21 16:47:14 +0800

    完善项目
    Signed-off-by: suigang <244153653@qq.com>

---
+++ src/page/mapList/mapList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="contact-us">
+        地图页面
+    </div>
+</template>
+<script>
+    export default {
+        name: 'contactUs',
+        components: {
+        },
+        data() {
+            return {
+            }
+        },
+        created() {
+        },
+        mounted() {
+        },
+        methods: {
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    @import '../../less/variable.less';
+    .contact-us {
+        width: 100%;
+        overflow: hidden;
+        height: 500px;
+        background: @bgColor;
+        color: @fontColor;
+    }
+</style>
+
